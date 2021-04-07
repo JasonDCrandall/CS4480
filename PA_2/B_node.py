75b4dfc203bb7e5eb599bb089797d8fa542b1cfd
Jason Crandall <u0726408@uccss-MacBook-Pro-3.local>
2021-04-07 09:11:35 -0600

    Commented B

--- PA_2/B_node.py
+++ PA_2/B_node.py
@@ -1,5 +1,5 @@
 # This is the node that will receive the filtered data and print the
-# received header of each message.
+# header of the first modified message from R
 #
 # Created By: Jason Crandall u0726408
 
@@ -8,37 +8,44 @@ import json
 import sys
 import threading
 
+# Define global variables
 HOST = 'localhost'
 PORT = 5555
 msgCount = 0
 
+# Main function that initializes B as a server listening for messages
+# from the Router. Prints the total messages received once complete.
 def main():
     startServer()
     print("Messages Received: ", msgCount)
-    # t = threading.Thread(target = startServer, daemon=True)
-    # t.start()
 
-    # # Initiate the client loop for a safe exit
-    # while True:
-    #     cmdInput = input("")
-    #     if cmdInput == "q":
-    #         exit()
 
+# startServer is a function that opens a UDP connection waiting for input.
+# Once a connection is made, it will read the data and print the header if 
+# it is the first modified message, otherwise it will increase the total message
+# count.
 def startServer():
     print("Listening for messages")
     global msgCount
     with socket(AF_INET, SOCK_DGRAM) as s:
         s.bind((HOST, PORT))
+        # Listen for incoming messages
         while True:
             data, addr = s.recvfrom(2048)
             command = ''
+
+            # Ensure the correct format is received
             try:
                 jsonData = json.loads(data.decode())
                 command = jsonData["command"]
             except:
                 print("Unaccepted Format")
+
+            # Close the connection once the router informs it to
             if(command == "terminate"):
                 return
+
+            # Get message from the router/parse header
             elif(command == "sendMessage"):
                 message = jsonData["message"]
                 sra = int(message[0:2], 16)
@@ -46,6 +53,8 @@ def startServer():
                 srp = int(message[4:6], 16)
                 dsp = int(message[6:8], 16)
                 msgCount += 1
+
+                # Print header if it is the first modified
                 if (jsonData["firstModified"]):
                     print('First Modified Header: ',sra,dsa,srp,dsp)
 
