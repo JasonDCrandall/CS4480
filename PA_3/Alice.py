9bf6d3fdd3ad9e69c6252f2e28f462aad6ed2ad2
JasonCrandall <jcrandall7@gmail.com>
2021-04-21 15:33:42 -0600

    Alice skeleton

--- PA_3/Alice.py
+++ PA_3/Alice.py
@@ -1,1 +1,11 @@
 # Code representing alice in the secure text transfer
+
+# Below are the steps needed from Alice's side of the program:
+
+# Request and verify Bob's public Key
+
+# Use crypto lib for integrity, encryption, and signing of a msg to send to bob
+
+# Transfer message with symmetric key
+
+# End
