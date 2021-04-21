cefa829809d59f81d69b71604fe4fc110fd068cd
JasonCrandall <jcrandall7@gmail.com>
2021-04-21 15:35:11 -0600

    Bob skeleton

--- PA_3/Bob.py
+++ PA_3/Bob.py
@@ -1,1 +1,14 @@
 # Code representing Bob in the secure text transfer
+
+# Below are the basic steps for Bob's side of the program:
+
+# Provide public key with signed msg digest
+
+# Wait
+
+# Receeive secure data from Alice
+
+# Use crypto lib to inverse Alice's msg and print
+
+# End
+
