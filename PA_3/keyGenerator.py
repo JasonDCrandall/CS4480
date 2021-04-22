ba709063127fd01064715dc2026ab9d89fd0fef9
JasonCrandall <jcrandall7@gmail.com>
2021-04-22 07:01:14 -0600

    t

--- PA_3/keyGenerator.py
+++ PA_3/keyGenerator.py
@@ -3,7 +3,7 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend)
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
 print(private_key, private_key.public_key())
 
 
