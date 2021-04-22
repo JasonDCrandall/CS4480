a79612e1b5cb4065b2ebb67771403175f5137f98
JasonCrandall <jcrandall7@gmail.com>
2021-04-22 06:52:34 -0600

    switched to cryptography

--- PA_3/keyGenerator.py
+++ PA_3/keyGenerator.py
@@ -1,8 +1,16 @@
 # Code for generating the public and private keys needed for the program
 
-from Crypto.PublicKey import RSA
+from cryptography.hazmat.primitives.asymmetric import rsa
 
-Akey = RSA.generate(2048)
-a_public = Akey.publickey().exportKey("PEM")
-a_private = Akey.exportKey("PEM")
-print(a_private, a_public)
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+print(private_key, private_key.public_key())
+
+
+
+
+# from Crypto.PublicKey import RSA
+
+# Akey = RSA.generate(2048)
+# a_public = Akey.publickey().exportKey("PEM")
+# a_private = Akey.exportKey("PEM")
+# print(a_private, a_public)
