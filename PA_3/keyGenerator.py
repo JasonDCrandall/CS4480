be7107c573be410c7f1de842bb6701fba68134a8
JasonCrandall <jcrandall7@gmail.com>
2021-04-22 06:59:09 -0600

    t

--- PA_3/keyGenerator.py
+++ PA_3/keyGenerator.py
@@ -1,8 +1,9 @@
 # Code for generating the public and private keys needed for the program
 
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=None)
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 print(private_key, private_key.public_key())
 
 
