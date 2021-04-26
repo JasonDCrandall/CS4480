1eb0696be186b587d5ed587135f218a6705ce51e
JasonCrandall <jcrandall7@gmail.com>
2021-04-26 00:06:40 -0600

    moved crypto code to individual files

--- PA_3/Alice.py
+++ PA_3/Alice.py
@@ -1,11 +1,26 @@
 # Code representing alice in the secure text transfer
+import os
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import utils
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from socket import *
 import json
 
 HOST = "localhost"
 BPORT = 3344
 APORT = 4455
+delim = bytearray(b'++++++++++')
 # Below are the steps needed from Alice's side of the program:
+
+def main():
+    bobInfo = requestBobsKey()
+    msg = buildMessage(bobInfo)
+    sendMsgToBob(msg)
+
 
 # Request and verify Bob's public Key
 def requestBobsKey():
@@ -21,20 +36,88 @@ def requestBobsKey():
         print('Sending Command: ',command)
         s.send(json.dumps(command).encode())
         data = s.recv(1024)
-        print('Received Data: ', data)
+        return data
+        
+def buildMessage(bob_msg):
+    # ********** Alice **********
+    # Verification from Alice ****
+    # Extract public key after delimeter
+    split_b_msg = bob_msg.split(b'++++++++++')
+    recv_b_key = split_b_msg[1]
+    recv_b_sig = split_b_msg[0]
+
+    # Alice takes encrypted msg, decrypt with c public key (from disk)
+    cPublicKey = load_public_key('/home/u0726408/cs4480/CS4480/PA_3/keys/cPublic.pem')
+    try:
+        aVerify = cPublicKey.verify(recv_b_sig, recv_b_key, padding.PSS(mgf=padding.MGF1(
+            hashes.SHA1()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1())
+    except:
+        print("Verification failed for received message")
+        exit()
+
+    # Result is bob public key (can compare with delimited part)
+    formatted_b_key = serialization.load_pem_public_key(
+        recv_b_key, default_backend())
+
+    # Print message to send to bob
+    string_message = "This is a test message"
+    print('Sending to bob: ', string_message)
+    message = string_message.encode()
+    byteMessage = bytearray(message)
+
+    # Read Alice private key from disk
+    aPrivateKey = load_private_key('/home/u0726408/cs4480/CS4480/PA_3/keys/alicePrivate.pem')
+
+    # Encrypt new hash with Alic private key
+    a_sig = bytearray(aPrivateKey.sign(message, padding.PSS(mgf=padding.MGF1(
+        algorithm=hashes.SHA1()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1()))
+
+    # Combine msg with ^
+    a_msg_arr = a_sig+delim+byteMessage
+    a_msg = bytes(a_msg_arr)
+
+    # Encrypt ^ with AES key --- store as A
+    ks = bytearray(os.urandom(32))
+    raw_ks = bytes(ks)
+    iv = b'a' * 16 # This is the initialization vector
+    cipher = Cipher(algorithms.AES(raw_ks), modes.CBC(iv), default_backend())
+    encryptor = cipher.encryptor()
+    ct = encryptor.update(a_msg)
+    a_alice_encryption = bytearray(ct)
+    raw_a_alice_encryption = bytes(a_alice_encryption)
+
+    # Encrypt AES key with Bob public key --- store as B
+    b_alice_encryption = bytearray(formatted_b_key.encrypt(raw_ks, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA1()),algorithm=hashes.SHA1(),label=None)))
+
+    # Concatonate A, B
+    full_a_msg = a_alice_encryption+delim+b_alice_encryption
+    raw_full_a_msg = bytes(full_a_msg)
+
+    return raw_full_a_msg
+
+def sendMsgToBob(msg):
+    with socket(AF_INET, SOCK_STREAM) as s:
         try:
-            jsonData = json.loads(data.decode())
+            s.connect((HOST, BPORT))
         except:
-            print(f"Bob gave an invalid response.")
-        # if jsonData["returnType"] == 'leaderElection':
-        #     returnedID = int(jsonData["id"])
-        #     node.fileList = jsonData["fileList"]
-        #     leaderID = returnedID
-# Use crypto lib for integrity, encryption, and signing of a msg to send to bob
+            print(f"Bob failed to respond.")
+            return
+        print('Sending Message')
+        s.send(msg)
 
-# Transfer message with symmetric key
+def load_public_key(filename):
+    with open(filename, "rb") as pem_in:
+        pemlines = pem_in.read()
+    public_key = serialization.load_pem_public_key(pemlines, default_backend())
+    return public_key
 
-# End
+
+def load_private_key(filename):
+    with open(filename, "rb") as pem_in:
+        pemlines = pem_in.read()
+    private_key = serialization.load_pem_private_key(
+        pemlines, None, default_backend())
+    return private_key
 
 
 if __name__ == "__main__":
--- PA_3/Bob.py
+++ PA_3/Bob.py
@@ -1,5 +1,103 @@
 # Code representing Bob in the secure text transfer
+import os
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import utils
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from socket import *
+import json
 
+delim = bytearray(b'++++++++++')
+HOST = 'localhost'
+PORT = 3344
+
+def main():
+    with socket(AF_INET, SOCK_DGRAM) as s:
+        s.bind((HOST, PORT))
+        # Listen for incoming messages
+        while True:
+            connection, addr = s.accept()
+            with connection:
+                data, addr = s.recvfrom(2048)
+                command = ''
+                # Ensure the correct format is received
+                try:
+                    jsonData = json.loads(data.decode())
+                    command = jsonData["command"]
+                except:
+                    command = data
+                if(command == "requestKey"):
+                    key_msg = getKeyInfo()
+                    connection.send(key_msg)
+                else:
+                    decryptMsg(command)
+                    exit()
+
+def getKeyInfo():
+    # ******** BOB *********
+    # Read bob public key from disk
+    bPublicKey = load_public_key('/home/u0726408/cs4480/CS4480/PA_3/keys/bobPublic.pem')
+    bPublicKey_bytes = bytearray(bPublicKey.public_bytes(encoding=serialization.Encoding.PEM,
+                                                        format=serialization.PublicFormat.SubjectPublicKeyInfo))
+    raw_b_public_key = bytes(bPublicKey_bytes)
+
+    # Sign ^ with c private key (from disk)
+    cPrivateKey = load_private_key('/home/u0726408/cs4480/CS4480/PA_3/keys/cPrivate.pem')
+    #b_sig = bytearray(cPrivateKey.sign(final_hash, padding.PSS(mgf=padding.MGF1(algorithm=hashes.SHA1()),salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1()))
+    b_sig = bytearray(cPrivateKey.sign(raw_b_public_key, padding.PSS(mgf=padding.MGF1(
+        algorithm=hashes.SHA1()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1()))
+    bob_msg_arr = b_sig+delim+bPublicKey_bytes
+    bob_msg = bytes(bob_msg_arr)
+    return bob_msg
+
+def decryptMsg(raw_full_a_msg):
+    # ********* BOB *************
+    # Break up A,B
+    split_a_msg = raw_full_a_msg.split(b'++++++++++')
+    first_e = split_a_msg[0]
+    second_e = split_a_msg[1]
+
+    # DO B FIRST: Decrypt AES key with Bob's private key
+    bPrivateKey = load_private_key('/home/u0726408/cs4480/CS4480/PA_3/keys/bobPrivate.pem')
+    aes_key = bPrivateKey.decrypt(second_e,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA1()),algorithm=hashes.SHA1(),label=None))
+
+    # NOW DO A: Decrypt A with AES key (split into msg, KA-(H(m)))
+    biv = b'a' * 16
+    decipher = Cipher(algorithms.AES(aes_key), modes.CBC(biv), default_backend())
+    decryptor = decipher.decryptor()
+    unencrypted_packet = decryptor.update(first_e)
+    print(unencrypted_packet)
+    # Bob gets Alic public key from disk
+    aPublicKey = load_public_key('/home/u0726408/cs4480/CS4480/PA_3/keys/alicePublic.pem')
+
+    # Decrypts second part of delimeted data with Alice public key
+    split_hash = unencrypted_packet.split(b'++++++++++')
+    a_hash = split_hash[0]
+    msg = split_hash[1]
+
+    try:
+        recv_msg = aPublicKey.verify(a_hash,msg,padding.PSS(mgf=padding.MGF1(algorithm=hashes.SHA1()),salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1())
+    except:
+        print("Unable to verify Alice's message")
+
+    print(msg)
+
+def load_public_key(filename):
+    with open(filename, "rb") as pem_in:
+        pemlines = pem_in.read()
+    public_key = serialization.load_pem_public_key(pemlines, default_backend())
+    return public_key
+
+
+def load_private_key(filename):
+    with open(filename, "rb") as pem_in:
+        pemlines = pem_in.read()
+    private_key = serialization.load_pem_private_key(
+        pemlines, None, default_backend())
+    return private_key
 # Below are the basic steps for Bob's side of the program:
 
 # Provide public key with signed msg digest
@@ -12,3 +110,5 @@
 
 # End
 
+if __name__ == "__main__":
+    main()
