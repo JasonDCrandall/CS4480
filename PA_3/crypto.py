628d67c8d95ad9e56c4805c04eb1da0b4ed1d0fa
JasonCrandall <jcrandall7@gmail.com>
2021-04-25 18:31:44 -0600

    receive bob msg

--- PA_3/crypto.py
+++ PA_3/crypto.py
@@ -28,33 +28,37 @@ bPublicKey = load_public_key('/home/u0726408/cs4480/CS4480/PA_3/keys/bobPublic.p
 #bPublicKey = load_public_key('keys\\bobPublic.pem')
 bPublicKey_bytes = bytearray(bPublicKey.public_bytes(encoding=serialization.Encoding.PEM,
                                            format=serialization.PublicFormat.SubjectPublicKeyInfo))
-
-# Hash bob public with sha1
-bob_hash = hashes.Hash(hashes.SHA1(), default_backend())
-bob_hash.update(bPublicKey_bytes)
-final_hash = bob_hash.finalize()
+raw_b_public_key = bytes(bPublicKey_bytes)
 
 # Sign ^ with c private key (from disk)
 cPrivateKey = load_private_key('/home/u0726408/cs4480/CS4480/PA_3/keys/cPrivate.pem')
 #cPrivateKey = load_private_key('keys\\cPrivate.pem')
-b_sig = bytearray(cPrivateKey.sign(final_hash, padding.PSS(mgf=padding.MGF1(algorithm=hashes.SHA1()),salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1()))
-bob_msg = b_sig+delim+bPublicKey_bytes
-# bob_bytearray = bytearray(b_sig)
-# bob_bytearray += delim
-# bob_bytearray += bPublicKey_bytes
-
-print(bytes(bob_msg))
-
-# # Bob sends public key KC-(H(KB+)), KB+
+#b_sig = bytearray(cPrivateKey.sign(final_hash, padding.PSS(mgf=padding.MGF1(algorithm=hashes.SHA1()),salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1()))
+b_sig = bytearray(cPrivateKey.sign(raw_b_public_key, padding.PSS(mgf=padding.MGF1(algorithm=hashes.SHA1()),salt_length=padding.PSS.MAX_LENGTH), hashes.SHA1()))
+bob_msg_arr = b_sig+delim+bPublicKey_bytes
+bob_msg = bytes(bob_msg_arr)
+# ****** Send bob message *****
 
 # Verification from Alice ****
 # Extract public key after delimeter
+split_b_msg = bob_msg.split(b'+++++')
+recv_b_key = split_b_msg[1]
+recv_b_sig = split_b_msg[0]
+
 # Alice takes encrypted msg, decrypt with c public key (from disk)
-# "unsha" the inner part with sha1
+cPublicKey = load_public_key('/home/u0726408/cs4480/CS4480/PA_3/keys/cPublic.pem')
+#cPublicKey = load_public_key('keys\\cPublic.pem')
+try:
+    aVerify = cPublicKey.verify(recv_b_sig,recv_b_key,padding.PSS(mgf=padding.MGF1(hashes.SHA1()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA1())
+except:
+    print("Verification failed for received message")
+    exit()
+
 # Result is bob public key (can compare with delimited part)
+formatted_b_key = serialization.load_pem_public_key(recv_b_key, default_backend())
+print(formatted_b_key)
 
 # Request user input for msg
-# Hash msg with sha1
 # Read Alice private key from disk
 # Encrypt new hash with Alic private key
 # Combine msg with ^
@@ -68,6 +72,5 @@ print(bytes(bob_msg))
 # NOW DO A: Decrypt A with AES key (split into msg, KA-(H(m)))
 # Bob gets Alic public key from disk
 # Decrypts second part of delimeted data with Alice public key
-# "unsha" ^
 # compare ^ with received msg
 # if match, display
